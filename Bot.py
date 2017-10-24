b35636f7718a8f974ea7f2c2d613879723799606
cruftboy <jasperhkw@gmail.com>
2017-10-24 14:43:40 -0700

    sinner test

--- Bot.py
+++ Bot.py
@@ -12,11 +12,15 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+	if('lord' in  message.author.display_name):
+		lord = message.author
+		await client.change_nickname(lord, "sinner")
+
 	if(message.author.id != client.user.id):
 	    messagelower = message.content
 	    messagelower = messagelower.lower()
 	    if('heck' in messagelower or 'hell' in messagelower or 'fuck' in messagelower or 'shit' in messagelower or 'cunt' in messagelower or 'bitch' in messagelower or 'ass' in messagelower or 'butt' in messagelower or 'bum' in messagelower or 'frick' in messagelower or 'damn' in messagelower or 'darn' in messagelower or 'dang' in messagelower or 'bastard' in messagelower or 'anus' in messagelower or 'gosh' in messagelower or 'fart' in messagelower or 'stupid' in messagelower or 'idiot' in messagelower):
 	    	await client.send_message(message.channel, 'Sorry sir, this is a christian server, so no swearing')
-	    elif('@' in message):
+	    elif('@' in messagelower):
 	    	await client.send_message(message.channel, 'I would rather @ the lord with my prayers!')
 client.run('MzcwMjg2MTAzMjkyNDExOTA3.DM_ZDQ.Lif3NSbl4aCBecwJ2qZl-6yuLV4')
