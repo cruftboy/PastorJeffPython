53c52afca0bf96c254db5d0970a3f717891a200f
cruftboy <jasperhkw@gmail.com>
2017-10-24 10:16:08 -0700

    upper case support

--- Bot.py
+++ Bot.py
@@ -12,9 +12,11 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-	if message.author.id != client.user.id:
-	    if('heck' in message.content or 'hell' in message.content or 'fuck' in message.content or 'shit' in message.content or 'cunt' in message.content or 'bitch' in message.content or 'ass' in message.content or 'butt' in message.content or 'bum' in message.content or 'frick' in message.content or 'damn' in message.content or 'darn' in message.content or 'dang' in message.content or 'bastard' in message.content or 'anus' in message.content or 'gosh' in message.content or 'fart' in message.content or 'stupid' in message.content or 'idiot' in message.content):
-	       await client.send_message(message.channel, 'Sorry sir, this is a christian server, so no swearing')
-	    elif('@' in message.content):
+	if(message.author.id != client.user.id):
+	    messagelower = message.content
+	    messagelower = messagelower.lower()
+	    if('heck' in messagelower or 'hell' in messagelower or 'fuck' in messagelower or 'shit' in messagelower or 'cunt' in messagelower or 'bitch' in messagelower or 'ass' in messagelower or 'butt' in messagelower or 'bum' in messagelower or 'frick' in messagelower or 'damn' in messagelower or 'darn' in messagelower or 'dang' in messagelower or 'bastard' in messagelower or 'anus' in messagelower or 'gosh' in messagelower or 'fart' in messagelower or 'stupid' in messagelower or 'idiot' in messagelower):
+	    	await client.send_message(message.channel, 'Sorry sir, this is a christian server, so no swearing')
+	    elif('@' in message):
 	    	await client.send_message(message.channel, 'I would rather @ the lord with my prayers!')
 client.run('MzcwMjg2MTAzMjkyNDExOTA3.DM_ZDQ.Lif3NSbl4aCBecwJ2qZl-6yuLV4')
