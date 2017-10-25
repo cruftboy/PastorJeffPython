cde708cf507f0c952d1965fbdbdf9386317dbf91
cruftboy <jasperhkw@gmail.com>
2017-10-25 10:01:03 -0700

    Add lucky boy

--- Bot.py
+++ Bot.py
@@ -1,26 +1,52 @@
 import discord
 import asyncio
+import random
+
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
+	print('Logged in as')
+	print(client.user.name)
+	print(client.user.id)
+	print('------')
+
+
 
 @client.event
 async def on_message(message):
+
+
+	members = []
+
+	for member in client.get_all_members():
+		members.append(member)
+
+	for member in members:
+		if('lucky boy' in member.display_name):
+			await client.change_nickname(member, None)
+
+		
+	lucky = random.choice(members)
+
+	await client.change_nickname(lucky, 'lucky boy')
+
+
+
+
 	if('lord' in  message.author.display_name):
 		lord = message.author
-		await client.change_nickname(lord, "sinner")
+		await client.change_nickname(lord, 'sinner')
+		await client.send_message(message.channel, 'Impersonating the lord is a sin, so you are a sinner')
 
 	if(message.author.id != client.user.id):
-	    messagelower = message.content
-	    messagelower = messagelower.lower()
-	    if('heck' in messagelower or 'hell' in messagelower or 'fuck' in messagelower or 'shit' in messagelower or 'cunt' in messagelower or 'bitch' in messagelower or 'ass' in messagelower or 'butt' in messagelower or 'bum' in messagelower or 'frick' in messagelower or 'damn' in messagelower or 'darn' in messagelower or 'dang' in messagelower or 'bastard' in messagelower or 'anus' in messagelower or 'gosh' in messagelower or 'fart' in messagelower or 'stupid' in messagelower or 'idiot' in messagelower):
-	    	await client.send_message(message.channel, 'Sorry sir, this is a christian server, so no swearing')
-	    elif('@' in messagelower):
-	    	await client.send_message(message.channel, 'I would rather @ the lord with my prayers!')
+		messagelower = message.content
+		messagelower = messagelower.lower()
+		if('heck' in messagelower or 'hell' in messagelower or 'fuck' in messagelower or 'shit' in messagelower or 'cunt' in messagelower or 'bitch' in messagelower or 'ass' in messagelower or 'butt' in messagelower or 'bum' in messagelower or 'frick' in messagelower or 'damn' in messagelower or 'darn' in messagelower or 'dang' in messagelower or 'bastard' in messagelower or 'anus' in messagelower or 'gosh' in messagelower or 'fart' in messagelower or 'stupid' in messagelower or 'idiot' in messagelower):
+			await client.send_message(message.channel, 'Sorry sir, this is a christian server, so no swearing')
+		elif('@' in messagelower):
+			await client.send_message(message.channel, 'I would rather @ the lord with my prayers!')
+		elif("who's here" in messagelower or "who is here" in messagelower):
+			await client.send_message(message.channel, members)
 client.run('MzcwMjg2MTAzMjkyNDExOTA3.DM_ZDQ.Lif3NSbl4aCBecwJ2qZl-6yuLV4')
